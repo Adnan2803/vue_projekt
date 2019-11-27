b6cd81cbea085d313db6a11c243bdecb76e767f1
Adnan2803 <58227443+Adnan2803@users.noreply.github.com>
2019-11-27 18:21:53 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <nav class="navbar navbar-light bg-light">
+          <div class="col-4">
+            <a class="navbar-brand" href="#">
+             <img src="@/assets/logo.svg" width="30" height="30" class="d-inline-block align-top" alt=""/>
+             Instagram
+            </a>
+          </div>
+          <div class="col-4">
+            <div id="search-center">
+                <div class="input-group">
+                    <input type="text" class="form-control input-lg" placeholder="Search" style="margin-left: 100px; margin-right: 100px;" />
+                </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <img src="@/assets/compass.svg" width="30" height="30" style="margin-right: 30px;" class="d-inline-block align-top" alt=""/>
+            <img src="@/assets/heart.svg" width="30" height="30" style="margin-right: 30px;" class="d-inline-block align-top" alt=""/>
+            <img src="@/assets/user.svg" width="30" height="30" class="d-inline-block align-top" alt=""/>
+          </div>
+      </nav>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+input{
+    text-align: center;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  border-bottom: 1px solid black;
+  a {
+    font-weight: bold;
+    color:rgba(0,0,0);
+    font-family: 'Kaushan Script', cursive;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Instagrammy</h1>
+    <p class="lead">Application better than Instagram</p>
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
